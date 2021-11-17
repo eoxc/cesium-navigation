b4810ec2f729ac114c74526d8760b5c0a422c6dd
baloola <baloola-mu@hotmail.com>
2021-11-17 13:48:19 +0100

    linting

--- Source/index.js
+++ Source/index.js
@@ -1,4 +1,3 @@
 var viewerCesiumNavigationMixin =require("cesium-navigation/Source/viewerCesiumNavigationMixin")
 require("./Styles/cesium-navigation.less");
-module.exports = viewerCesiumNavigationMixin
-// export default viewerCesiumNavigationMixin
+module.exports = viewerCesiumNavigationMixin;
